6ecccc777eb038e0871250abb7af00cb64ad1742
zhaogaoxing <865072432@qq.com>
2016-09-10 20:43:39 +0800

    添加上下左右的控制键，方向的变化

--- Classes/GameLayer.cpp
+++ Classes/GameLayer.cpp
@@ -29,12 +29,74 @@ bool GameLayer::init()
 
 	Size visibleSize = Director::getInstance()->getVisibleSize();
 	Vec2 origin = Director::getInstance()->getVisibleOrigin();
+	SetAppearance();
+
+	return true;
+}
+
+void GameLayer::SetAppearance()
+{
+	Size visibleSize = Director::getInstance()->getVisibleSize();
 
 	Sprite* background = Sprite::create("bg.png");
 	background->setPosition(Vec2(visibleSize.width / 2, visibleSize.height / 2));
-	this->addChild(background);
+	this->addChild(background,0);//±³¾°
+
+	MenuItemImage* butup = MenuItemImage::create("CloseNormal.png", "CloseSelected.png",
+		CC_CALLBACK_1(GameLayer::SetDirection, this, up));
+	butup->setPosition(Vec2(visibleSize.width - 100, visibleSize.height / 2 - 100));
+	
+	MenuItemImage* butdown = MenuItemImage::create("CloseNormal.png", "CloseSelected.png",
+		CC_CALLBACK_1(GameLayer::SetDirection, this, down));
+	butdown->setPosition(Vec2(visibleSize.width - 100, visibleSize.height / 2 - 150));
+
+	MenuItemImage* butright = MenuItemImage::create("CloseNormal.png", "CloseSelected.png",
+		CC_CALLBACK_1(GameLayer::SetDirection, this, right));
+	butright->setPosition(Vec2(visibleSize.width - 50, visibleSize.height / 2 - 125));
+
+	MenuItemImage* butleft = MenuItemImage::create("CloseNormal.png", "CloseSelected.png",
+		CC_CALLBACK_1(GameLayer::SetDirection, this, left));
+	butleft->setPosition(Vec2(visibleSize.width - 150, visibleSize.height / 2 - 125));
 
 
 
-	return true;
+	Menu* menu = Menu::create(butup, butdown, butright, butleft, NULL);
+	menu->setPosition(Vec2::ZERO);
+	this->addChild(menu, 2);
+
+
+}
+
+void GameLayer::SetDirection(Ref* pSender, Direction direc)
+{
+	Direction headdir = head->getDirec();
+	switch (direc)
+	{
+	case up:
+		if (headdir == left || headdir == right)
+		{
+			head->setDirec(up);
+		}
+		break;
+	case down:
+		if (headdir == left || headdir == right)
+		{
+			head->setDirec(down);
+		}
+		break;
+	case right:
+		if (headdir == up || headdir == down)
+		{
+			head->setDirec(right);
+		}
+		break;
+	case left:
+		if (headdir == up || headdir == down)
+		{
+			head->setDirec(left);
+		}
+		break;
+	default:
+		break;
+	}
 }
--- Classes/GameLayer.h
+++ Classes/GameLayer.h
@@ -1,6 +1,9 @@
 #ifndef _Game_Layer_
 #define _Game_Layer_
+
 #include"cocos2d.h"
+#include"SnakeHead.h"
+
 USING_NS_CC;
 
 class GameLayer : public cocos2d::Layer
@@ -10,6 +13,15 @@ public:
 	static cocos2d::Scene* createScene();
 
 	virtual bool init();
+
+	SnakeHead* head;
+	Vector<Snake*>body;
+
+
+	void SetAppearance();
+	void SetDirection(Ref* pSender, Direction direc);
+	/*void PauseGame();
+	void StartGame();*/
 
 
 	// implement the "static create()" method manually
